bf600f32cd9ac36025b864e302cd1fab79c1279f
caklimas <caklimas@gmail.com>
2021-08-17 23:58:20 -0400

    Implement fluent api on Matrix

--- src/matrix/tests/transformation.rs
+++ src/matrix/tests/transformation.rs
@@ -1,10 +1,10 @@
 use std::f64::consts::PI;
-
-use crate::{matrix::{Matrix, axis::Axis}, tuple::Tuple};
+use crate::matrix::{Matrix, transformation::*};
+use crate::{matrix::{axis::Axis}, tuple::Tuple};
 
 #[test]
 fn translation_test() {
-    let translation = Matrix::translation(5.0, -3.0, 2.0);
+    let translation = translate(5.0, -3.0, 2.0);
     let point = Tuple::point(-3.0, 4.0, 5.0);
     let vector = Tuple::vector(-3.0, 4.0, 5.0);
     let inverse = translation.inverse();
@@ -16,19 +16,19 @@ fn translation_test() {
 
 #[test]
 fn scaling_test() {
-    let transform = Matrix::scaling(2.0, 3.0, 4.0);
+    let transform = scale(2.0, 3.0, 4.0);
     let point = Tuple::point(-4.0, 6.0, 8.0);
     let actual = transform.multiply_tuple(&point);
 
     assert_eq!(Tuple::point(-8.0, 18.0, 32.0), actual);
 
-    let transform = Matrix::scaling(2.0, 3.0, 4.0);
+    let transform = scale(2.0, 3.0, 4.0);
     let vector = Tuple::vector(-4.0, 6.0, 8.0);
     let actual = transform.multiply_tuple(&vector);
 
     assert_eq!(Tuple::vector(-8.0, 18.0, 32.0), actual);
 
-    let transform = Matrix::scaling(2.0, 3.0, 4.0);
+    let transform = scale(2.0, 3.0, 4.0);
     let inverse = transform.inverse();
     let vector = Tuple::vector(-4.0, 6.0, 8.0);
     let actual = inverse.multiply_tuple(&vector);
@@ -38,7 +38,7 @@ fn scaling_test() {
 
 #[test]
 fn reflect_test() {
-    let transform = Matrix::reflect(Axis::X);
+    let transform = reflect(Axis::X);
     let point = Tuple::point(2.0, 3.0, 4.0);
     let actual = transform.multiply_tuple(&point);
 
@@ -48,8 +48,8 @@ fn reflect_test() {
 #[test]
 fn rotate_x_test() {
     let point = Tuple::point(0.0, 1.0, 0.0);
-    let half_quarter = Matrix::rotate_x(PI / 4.0);
-    let full_quarter = Matrix::rotate_x(PI / 2.0);
+    let half_quarter = rotate_x(PI / 4.0);
+    let full_quarter = rotate_x(PI / 2.0);
     let half_quarter_actual = half_quarter.multiply_tuple(&point);
     let full_quarter_actual = full_quarter.multiply_tuple(&point);
     let half_quarter_result = (2.0f64).sqrt() / 2.0;
@@ -63,8 +63,8 @@ fn rotate_x_test() {
 #[test]
 fn rotate_y_test() {
     let point = Tuple::point(0.0, 0.0, 1.0);
-    let half_quarter = Matrix::rotate_y(PI / 4.0);
-    let full_quarter = Matrix::rotate_y(PI / 2.0);
+    let half_quarter = rotate_y(PI / 4.0);
+    let full_quarter = rotate_y(PI / 2.0);
     let half_quarter_actual = half_quarter.multiply_tuple(&point);
     let full_quarter_actual = full_quarter.multiply_tuple(&point);
     let half_quarter_result = (2.0f64).sqrt() / 2.0;
@@ -76,8 +76,8 @@ fn rotate_y_test() {
 #[test]
 fn rotate_z_test() {
     let point = Tuple::point(0.0, 1.0, 0.0);
-    let half_quarter = Matrix::rotate_z(PI / 4.0);
-    let full_quarter = Matrix::rotate_z(PI / 2.0);
+    let half_quarter = rotate_z(PI / 4.0);
+    let full_quarter = rotate_z(PI / 2.0);
     let half_quarter_actual = half_quarter.multiply_tuple(&point);
     let full_quarter_actual = full_quarter.multiply_tuple(&point);
     let half_quarter_result = (2.0f64).sqrt() / 2.0;
@@ -88,43 +88,43 @@ fn rotate_z_test() {
 
 #[test]
 fn shearing_test() {
-    let shearing = Matrix::shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    let shear = shear(1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(5.0, 3.0, 4.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(5.0, 3.0, 4.0), shear.multiply_tuple(&point));
 
-    let shearing = Matrix::shearing(0.0, 1.0, 0.0, 0.0, 0.0, 0.0);
+    let shear = shear(0.0, 1.0, 0.0, 0.0, 0.0, 0.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(6.0, 3.0, 4.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(6.0, 3.0, 4.0), shear.multiply_tuple(&point));
 
-    let shearing = Matrix::shearing(0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
+    let shear = shear(0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(2.0, 5.0, 4.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(2.0, 5.0, 4.0), shear.multiply_tuple(&point));
 
-    let shearing = Matrix::shearing(0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+    let shear = shear(0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(2.0, 7.0, 4.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(2.0, 7.0, 4.0), shear.multiply_tuple(&point));
 
-    let shearing = Matrix::shearing(0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    let shear = shear(0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(2.0, 3.0, 6.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(2.0, 3.0, 6.0), shear.multiply_tuple(&point));
 
-    let shearing = Matrix::shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+    let shear = shear(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     let point = Tuple::point(2.0, 3.0, 4.0);
     
-    assert_eq!(Tuple::point(2.0, 3.0, 7.0), shearing.multiply_tuple(&point));
+    assert_eq!(Tuple::point(2.0, 3.0, 7.0), shear.multiply_tuple(&point));
 }
 
 #[test]
 fn chaining_transformations() {
     let p = Tuple::point(1.0, 0.0, 1.0);
-    let a = Matrix::rotate_x(PI / 2.0);
-    let b = Matrix::scaling(5.0, 5.0, 5.0);
-    let c = Matrix::translation(10.0, 5.0, 7.0);
+    let a = rotate_x(PI / 2.0);
+    let b = scale(5.0, 5.0, 5.0);
+    let c = translate(10.0, 5.0, 7.0);
 
     let p2 = a.multiply_tuple(&p);
     assert_eq!(Tuple::point(1.0, -1.0, 0.0), p2);
@@ -139,11 +139,18 @@ fn chaining_transformations() {
 #[test]
 fn chained_transformation_reverse_order() {
     let p = Tuple::point(1.0, 0.0, 1.0);
-    let a = Matrix::rotate_x(PI / 2.0);
-    let b = Matrix::scaling(5.0, 5.0, 5.0);
-    let c = Matrix::translation(10.0, 5.0, 7.0);
+    let a = rotate_x(PI / 2.0);
+    let b = scale(5.0, 5.0, 5.0);
+    let c = translate(10.0, 5.0, 7.0);
 
     let t = c.multiply(&b.multiply(&a));
     
     assert_eq!(Tuple::point(15.0, 0.0, 7.0), t.multiply_tuple(&p));
+}
+
+#[test]
+fn fluent_api() {
+    let p = Tuple::point(1.0, 0.0, 1.0);
+    let transform = Matrix::identity(4).rotate_x(PI / 2.0).scale(5.0, 5.0, 5.0).translate(10.0, 5.0, 7.0);
+    assert_eq!(Tuple::point(15.0, 0.0, 7.0), transform.multiply_tuple(&p));
 }
--- src/matrix/transformation.rs
+++ src/matrix/transformation.rs
@@ -1,80 +1,108 @@
 use super::{Matrix, axis::Axis};
 
+pub fn translate(x: f64, y: f64, z: f64) -> Matrix {
+    let mut translation = Matrix::identity(4);
+    translation.elements[0][3] = x;
+    translation.elements[1][3] = y;
+    translation.elements[2][3] = z;
+
+    translation
+}
+
+pub fn scale(x: f64, y: f64, z: f64) -> Matrix {
+    let mut scaling = Matrix::identity(4);
+    scaling.elements[0][0] = x;
+    scaling.elements[1][1] = y;
+    scaling.elements[2][2] = z;
+
+    scaling
+}
+
+pub fn reflect(axis: Axis) -> Matrix {
+    return match axis {
+        Axis::X => scale(-1.0, 1.0, 1.0),
+        Axis::Y => scale(1.0, -1.0, 1.0),
+        Axis::Z => scale(1.0, 1.0, -1.0)
+    };
+}
+
+pub fn rotate_x(radians: f64) -> Matrix {
+    let mut rotation = Matrix::identity(4);
+    let cos = radians.cos();
+    let sin = radians.sin();
+    rotation.elements[1][1] = cos;
+    rotation.elements[1][2] = -sin;
+    rotation.elements[2][1] = sin;
+    rotation.elements[2][2] = cos;
+
+    rotation
+
+}
+
+pub fn rotate_y(radians: f64) -> Matrix {
+    let mut rotation = Matrix::identity(4);
+    let cos = radians.cos();
+    let sin = radians.sin();
+    rotation.elements[0][0] = cos;
+    rotation.elements[0][2] = sin;
+    rotation.elements[2][0] = -sin;
+    rotation.elements[2][2] = cos;
+
+    rotation
+
+}
+
+pub fn rotate_z(radians: f64) -> Matrix {
+    let mut rotation = Matrix::identity(4);
+    let cos = radians.cos();
+    let sin = radians.sin();
+    rotation.elements[0][0] = cos;
+    rotation.elements[0][1] = -sin;
+    rotation.elements[1][0] = sin;
+    rotation.elements[1][1] = cos;
+
+    rotation
+
+}
+
+pub fn shear(xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Matrix {
+    let mut shearing = Matrix::identity(4);
+    shearing.elements[0][1] = xy;
+    shearing.elements[0][2] = xz;
+    shearing.elements[1][0] = yx;
+    shearing.elements[1][2] = yz;
+    shearing.elements[2][0] = zx;
+    shearing.elements[2][1] = zy;
+
+    shearing
+}
+
 impl Matrix {
-    pub fn translation(x: f64, y: f64, z: f64) -> Self {
-        let mut translation = Self::identity(4);
-        translation.elements[0][3] = x;
-        translation.elements[1][3] = y;
-        translation.elements[2][3] = z;
-
-        translation
+    pub fn translate(&self, x: f64, y: f64, z: f64) -> Self {
+        translate(x, y, z).multiply(self)
     }
 
-    pub fn scaling(x: f64, y: f64, z: f64) -> Self {
-        let mut scaling = Self::identity(4);
-        scaling.elements[0][0] = x;
-        scaling.elements[1][1] = y;
-        scaling.elements[2][2] = z;
-
-        scaling
+    pub fn scale(&self, x: f64, y: f64, z: f64) -> Self {
+        scale(x, y, z).multiply(self)
     }
 
-    pub fn reflect(axis: Axis) -> Self {
-        return match axis {
-            Axis::X => Self::scaling(-1.0, 1.0, 1.0),
-            Axis::Y => Self::scaling(1.0, -1.0, 1.0),
-            Axis::Z => Self::scaling(1.0, 1.0, -1.0)
-        };
+    pub fn reflect(&self, axis: Axis) -> Self {
+        reflect(axis).multiply(self)
     }
 
-    pub fn rotate_x(radians: f64) -> Self {
-        let mut rotation = Self::identity(4);
-        let cos = radians.cos();
-        let sin = radians.sin();
-        rotation.elements[1][1] = cos;
-        rotation.elements[1][2] = -sin;
-        rotation.elements[2][1] = sin;
-        rotation.elements[2][2] = cos;
-
-        rotation
-
+    pub fn rotate_x(&self, radians: f64) -> Self {
+        rotate_x(radians).multiply(self)
     }
 
-    pub fn rotate_y(radians: f64) -> Self {
-        let mut rotation = Self::identity(4);
-        let cos = radians.cos();
-        let sin = radians.sin();
-        rotation.elements[0][0] = cos;
-        rotation.elements[0][2] = sin;
-        rotation.elements[2][0] = -sin;
-        rotation.elements[2][2] = cos;
-
-        rotation
-
+    pub fn rotate_y(&self, radians: f64) -> Self {
+        rotate_y(radians).multiply(self)
     }
 
-    pub fn rotate_z(radians: f64) -> Self {
-        let mut rotation = Self::identity(4);
-        let cos = radians.cos();
-        let sin = radians.sin();
-        rotation.elements[0][0] = cos;
-        rotation.elements[0][1] = -sin;
-        rotation.elements[1][0] = sin;
-        rotation.elements[1][1] = cos;
-
-        rotation
-
+    pub fn rotate_z(&self, radians: f64) -> Self {
+        rotate_z(radians).multiply(self)
     }
 
-    pub fn shearing(xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Self {
-        let mut shearing = Self::identity(4);
-        shearing.elements[0][1] = xy;
-        shearing.elements[0][2] = xz;
-        shearing.elements[1][0] = yx;
-        shearing.elements[1][2] = yz;
-        shearing.elements[2][0] = zx;
-        shearing.elements[2][1] = zy;
-
-        shearing
+    pub fn shear(&self, xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Self {
+        shear(xy, xz, yx, yz, zx, zy).multiply(self)
     }
 }
